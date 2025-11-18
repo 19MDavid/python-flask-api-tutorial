aa22b52171f65b881af02d846cdaae8f8e3e4b06
Mariana David <marianadavid10@yahoo.com>
2025-11-18 18:11:49 +0000

    proyecto python

--- src/app.py
+++ src/app.py
@@ -1,31 +1,44 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request 
+
 app = Flask(__name__)
 
 todos = [
     { "label": "My first task", "done": False }
 ]
 
+from flask import Flask, jsonify, request 
+
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False }
+]
+
+
+from flask import Flask, jsonify, request 
+
+
+todos = [
+    { "label": "My first task", "done": False } 
+]
+
+app = Flask(__name__)
+
+
 @app.route('/todos', methods=['GET'])
-def get_todos():
+def hello_world(): 
     return jsonify(todos)
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
-    print("Incoming request:", request_body)
-
-    todos.append(request_body) 
-
-    return jsonify(todos), 200
+    todos.append(request_body)
+    return jsonify(todos)
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    print("Deleting task at position:", position)
-
     todos.pop(position)
-
-    return jsonify(todos), 200 
-
+    return jsonify(todos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
